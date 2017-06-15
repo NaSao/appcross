2fcb1c123701334cf9993999d1a773858bb31778
NaSao <nayuqi@gmail.com>
2017-06-16 00:50:53 +0200

    updatge

---
+++ scancode.py
@@ -0,0 +1,65 @@
+# [START imports]
+import os
+import urllib2
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+#from google.appengine.api import urlfetch
+
+import jinja2
+import webapp2
+import uuid
+
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+
+
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        duuid = self.request.get('uuid')
+        template_values = {
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('qcode.html')
+        self.response.write(template.render(template_values))
+
+#         template = JINJA_ENVIRONMENT.get_template('register.html')
+#         self.response.write(template.render(template_values))
+        
+# [END main_page]
+
+# [START QCode]
+class QCode(webapp2.RequestHandler):
+
+    def post(self):
+        duuid = self.request.get('uuid')
+        template_values = {}
+#         template_values = {
+#             'user': user,
+#             'greetings': greetings,
+#             'guestbook_name': urllib.quote_plus(guestbook_name),
+#             'url': url,
+#             'url_linktext': url_linktext,
+#         }
+#       template = JINJA_ENVIRONMENT.get_template('register.html')
+        template = JINJA_ENVIRONMENT.get_template('qcode.html')
+        self.response.write(template.render(template_values))
+# [END QCode]
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/qcode', QCode),
+], debug=True)
+# [END app]
